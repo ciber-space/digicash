f3a8eb137db72212b05fd42f545fe0e6acfbc7c9
Digicash <92608820+digi-cash@users.noreply.github.com>
2021-10-15 15:49:59 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-node bin/digicash.js --peers http://peer.digicash.me:3001
+# Edit the 'myipaddress' to your hostname or public IP so it can
+# connect with peers.
+node bin/digicash.js --a myipaddress --peers http://peer.digicash.me:3001
